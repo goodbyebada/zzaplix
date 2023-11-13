21ea23d7e36ca42a03e5b057c37561d7bf7af609
goodbyebada <goodbyebada@naver.com>
2023-11-13 13:34:04 +0900

    home detail css 수정

--- src/routes/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
- const apiKey = "AIzaSyBsUlzD25HkoZHs8O9Q70QYDTLfkhzgMNs";
-
-  const url = `https://www.googleapis.com/youtube/v3/search?order=viewCount&part=snippet
-  &regionCode=KR&maxResults=100&q=${searchTitle}&type=video&key=${apiKey}`;
